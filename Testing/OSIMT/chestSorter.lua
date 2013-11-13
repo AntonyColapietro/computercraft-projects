5d598f46ba187d7849a19d7276393a8880ede193
Tony61989 <antn89@gmail.com>
2013-11-13 21:24:06 +0100

    implemented chestSorting function and added check for number of items left

--- Testing/OSIMT/chestSorter.lua
+++ Testing/OSIMT/chestSorter.lua
@@ -4,13 +4,22 @@ slots={}
 first=1
 second=2  -- slot 1 is for the chest (chests) used for sorting...
 last=16 -- last slot of the turtle variable
+numberOfItems=0
 
 function sortAllItems()
 chest=peripheral.wrap("front")
 chest.condenseItems()
 slots=chest.getAllStacks()
 getNames(slots)
+numberOfItems=#names
 cleanscrap()
+if(numberOfItems<12) then
+
+for index=first,numberOfItems do
+turtle.suck()
+end
+turtle.dig()
+end
 end 
  
 
@@ -31,13 +40,25 @@ function cleanscrap() --remove elements with name in the bad name list
     for b=1,#badnames do
          if(badnames[b]==names[o]) then
           chest.destroyStack(o)
-         end
+           numberOfItems=numberOfItems-1
+          end
      end  
      end
 
 end
 
 
+function chestSorting()
+
+turtle.select(first)
+turtle.turnLeft()
+turtle.turnLeft()
+turtle.place()
+dropAll()
+sortAllItems()
+turtle.turnLeft()
+turtle.turnLeft()
+end
 
 --drop all items in inventory in the chest in front of the turtle
 function dropAll()
