fe0b9a8e78dc5d7b6afe2d538668401b92c61dbd
Tony61989 <antn89@gmail.com>
2013-11-13 21:46:52 +0100

    comments and emptySlots ausiliar function

--- Testing/OSIMT/chestSorter.lua
+++ Testing/OSIMT/chestSorter.lua
@@ -2,7 +2,7 @@ badnames={"Cobblestone", "Dirt", "Gravel","Flint"} --filter list for undesired o
 names={}
 slots={}
 first=1
-second=2  -- slot 1 is for the chest (chests) used for sorting...
+second=2  -- slot 1 is for the chest (chests) used for sorting...(chests should be as many as the blocks the turtle is gonna dig...)
 last=16 -- last slot of the turtle variable
 numberOfItems=0
 
@@ -13,16 +13,15 @@ slots=chest.getAllStacks()
 getNames(slots)
 numberOfItems=#names
 cleanscrap()
-if(numberOfItems<12) then
-
-for index=first,numberOfItems do
-turtle.suck()
-end
-turtle.dig()
-end
+  if(numberOfItems<13) then
+     for index=first,numberOfItems do
+        turtle.suck()
+      end
+    turtle.dig()
+  end
 end 
  
-
+--get list of the items in the chest
   function getNames(invslots) 
   i=first 
    for k,v in pairs(invslots) do    
@@ -47,7 +46,7 @@ function cleanscrap() --remove elements with name in the bad name list
 
 end
 
-
+--main function : THIS is the only method that external programs should call
 function chestSorting()
 
 turtle.select(first)
@@ -59,6 +58,17 @@ sortAllItems()
 turtle.turnLeft()
 turtle.turnLeft()
 end
+
+function emptySlots()
+emptySlotsLeft=0
+for i=second,last do
+    if(turtle.getItemCount(i)==0) then
+      emptySlotsLeft=emptySlotsLeft+1
+     end
+  end
+return emptySlotsLeft
+end
+
 
 --drop all items in inventory in the chest in front of the turtle
 function dropAll()
