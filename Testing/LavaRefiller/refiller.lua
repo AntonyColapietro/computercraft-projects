8a80d9a298a206ed0e51a0baf116ab99d4edede8
Tony61989 <antn89@gmail.com>
2013-11-16 20:44:20 +0100

    implemented new version of the function refill() : now the turtle will get fuel from every side at each step of a path of maxLenght and repeat this for a maxRepetitions time .

--- Testing/LavaRefiller/refiller.lua
+++ Testing/LavaRefiller/refiller.lua
@@ -1,23 +1,62 @@
 dofile "walker.lua"
 P=D
+maxLenght=16
+maxRepetitions=2
+
 function refill()
-for u=1,15 do
-move(W)
-while(turtle.placeDown()) do
-turtle.refuel()
-move(W)
-end
-move(W)
-move(P)
-move(P)
-invert()
+  move(Q)
+  move(Q)
+	for mr=1,maxRepetitions do
+		for ml=1,maxLenght do
+      		if(turtle.place()) then
+         		turtle.refuel()        --first refill from in front of the turtle
+
+            	move(W)
+
+         		if(turtle.placeUp()) then  --refill from the layer above
+          		turtle.refuel()
+           		end
+         		if(turtle.placeDown()) then  --refill from the bottom layer
+         		turtle.refuel()
+          		end
+
+        		 turtle.turnRight()
+         		
+         		if(turtle.place()) then        --refill from the right side
+         		turtle.refuel()
+          		end
+
+         		turtle.turnLeft()
+        	    turtle.turnLeft()
+
+         		if(turtle.place()) then          --refill from the left side
+         		turtle.refuel()
+          		end
+
+          		turtle.turnRight()      --turn in the original orientation
+       		
+       	     else
+              move(W)
+
+           end
+       end
+        if(mr<2) then
+         move(W)
+         move(P)
+         move(W)
+         move(W)
+         move(P)
+         invert() 
+        end
+   end
+  move(E)
+  move(E)
 end
 
-end
 function invert()
-if(P==D) then
- P=A
-else
-P=D
-end
+  if(P==D) then
+     P=A
+   else
+    P=D
+   end
 end
